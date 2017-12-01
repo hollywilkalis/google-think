5b2fcd756d1cc8b0f7c9a56cfe7a494939720a79
Holly Wilkalis <holly.wilkalis@gmail.com>
2017-12-01 14:40:32 -0800

    Add additional styling, and content for hero box

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
   $(".dropdown-button").dropdown();
 
+  // Initialize collapse button
+    $(".button-collapse").sideNav();
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
 });
